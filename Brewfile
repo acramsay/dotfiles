f77ca32bbd51690412ac71a58fbfdca8b3079ee2
Alex Ramsay <acramsay@proton.me>
2025-03-10 00:25:29 -0400

    feat: refactored. Much simpler now

--- Brewfile
+++ Brewfile
@@ -1,11 +1,17 @@
 brew "zellij"
+brew "ranger"
 
 brew "helix"
 brew "yaml-language-server"
 brew "yamlfmt"
 brew "go"
+brew "delve"
+brew "gofumpt"
+brew "gopls"
+brew "dprint"
 
 # dev tools
+brew "git"
 brew "go-task"
 brew "lazygit"
 brew "pre-commit"
@@ -13,13 +19,20 @@ brew "pre-commit"
 # terraform
 tap "hashicorp/tap"
 brew "hashicorp/tap/terraform"
+brew "terragrunt"
 
-# k8s
-cask "rancher"
+# k8s tools
+cask "rancher" # not available on linux
 brew "kustomize"
 brew "k3d"
 brew "k9s"
 brew "tilt"
+
+# if OS.linux?
+  # cask "discord" # not available on linux
+  # cask "librewolf" # not available on linux
+  # cask "steam" # not available on linux
+# end
 
 if OS.mac?
   cask "slack"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,6 @@
-# Create global taskfile
-if [ ! -f ~/Taskfile.yaml ]; then
-	echo blash
-	cat <<-EOF > ~/Taskfile.yaml
-	version: '3'
-
-	includes:
-	dotfiles:
-		taskfile: $(dirname $(realpath $0))/Taskfile.yaml
-		flatten: true
-	EOF
+# Install hombrew
+if ! type "brew" >/dev/null 2>&1; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [[ -f /etc/os-release ]]; then
-	source /etc/os-release
-fi
-
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-	ln -sf $(dirname $(realpath $0))/package/Brewfile ~/Brewfile
-
-	if ! type "bash" >/dev/null 2>&1; then
-		# Install hombrew
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	fi
-	
-	if ! type "task" >/dev/null 2>&1; then
-		brew install go-task
-	fi
-fi
-
-# if [[ "$ID" == "opensuse-tumbleweed" ]]; then
-# 	# TODO
-# fi
+brew bundle --file $(dirname $(realpath $0))/home/Brewfile
