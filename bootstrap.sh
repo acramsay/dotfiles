94f2a3a24e7d744e89b846848dc9b56f3eb9f0c3
Alex Ramsay <acramsay@proton.me>
2025-03-10 00:34:35 -0400

    feat: cleanup and added update task

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ if ! type "brew" >/dev/null 2>&1; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew bundle --file $(dirname $(realpath $0))/home/Brewfile
+brew bundle --file $(dirname $(realpath $0))/Brewfile
