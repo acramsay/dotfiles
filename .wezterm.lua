e4e766a44338d9f5ad8113b2593ee7ec8ac48738
Alex Ramsay <acramsay@proton.me>
2025-03-23 12:35:13 -0400

    feat: added background and transparent bg

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,10 +2,15 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Macchiato'
--- config.font = wezterm.font("Comic Code", {weight="Bold"})
 
--- config.font = wezterm.font("Rec Mono Casual", {weight="Regular", stretch="Normal", style="Normal"})
-config.font = wezterm.font("Rec Mono Casual", {weight="Regular", stretch="Normal", style="Italic"})
+config.font = wezterm.font("Rec Mono Casual", {weight="Regular", stretch="Normal", style="Normal"})
+-- config.font = wezterm.font("Rec Mono Casual", {weight="Regular", stretch="Normal", style="Italic"})
 config.font_size = 14
+config.line_height = 1.2
+
+config.window_background_image = 'fire.jpg'
+config.window_background_image_hsb = {
+  brightness = 0.2,
+}
 
 return config
